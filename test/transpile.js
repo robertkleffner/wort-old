dcdb7cefec518f477ef9ce1c34425f81d5fe7f81
Rob Kleffner <gamecodeshark@gmail.com>
2014-08-22 19:15:10 -0600

    Rewrote transpiler to use SourceMap

--- test/transpile.js
+++ test/transpile.js
@@ -7,72 +7,72 @@ var should = require('chai').should(),
 describe('#transpile', function() {
     it('should return an empty module for empty input', function() {
         var res = transpile.transpile(s.analyze(p.parse(l.lex(''), 'hodor')));
-        res.should.equal('module.exports = (function() {\n})();\n');
+        res.toString().should.equal('module.exports = (function() {})();');
     });
 
     it('should emit require statements', function() {
         var res = transpile.transpile(s.analyze(p.parse(l.lex('require "test" as t;'), 'hodor')));
-        res.should.equal('var t = require("test");\nmodule.exports = (function() {\n})();\n');
+        res.toString().should.equal('var t = require("test");module.exports = (function() {})();');
     });
 
     it('should emit a simple public definition', function() {
         var res = transpile.transpile(s.analyze(p.parse(l.lex('Test: 2;'), 'hodor')));
-        res.should.equal('module.exports = (function() {\nvar $0 = {};\n$0.Test = function(stack) {\nstack.push(2);\n};\nreturn $0;\n})();\n');
+        res.toString().should.equal('module.exports = (function() {var $0 = {};$0.Test = function(stack) {stack.push(2);};return $0;})();');
     });
 
     it('should emit a public and private definition', function() {
         var res = transpile.transpile(s.analyze(p.parse(l.lex('-test: "hello"; test: 2;'), 'hodor')));
-        res.should.equal('module.exports = (function() {\nvar $0 = {};\nfunction _test(stack) {\nstack.push("hello");\n}\n' +
-            '$0.test = function(stack) {\nstack.push(2);\n};\nreturn $0;\n})();\n');
+        res.toString().should.equal('module.exports = (function() {var $0 = {};function _test(stack) {stack.push("hello");}' +
+            '$0.test = function(stack) {stack.push(2);};return $0;})();');
     });
 
     it('should emit an object literal', function() {
         var res = transpile.transpile(s.analyze(p.parse(l.lex('Test: {test: "hello"};'), 'hodor')));
-        res.should.equal('module.exports = (function() {\nvar $0 = {};\n$0.Test = function(stack) {\nstack.push({test: "hello"});\n};\nreturn $0;\n})();\n');
+        res.toString().should.equal('module.exports = (function() {var $0 = {};$0.Test = function(stack) {stack.push({test: "hello"});};return $0;})();');
     });
 
     it('should emit inline javascript', function() {
         var res = transpile.transpile(s.analyze(p.parse(l.lex('Test: ~~~ console.log("hodor!") ~~~;'), 'hodor')));
-        res.should.equal('module.exports = (function() {\nvar $0 = {};\n$0.Test = function(stack) {\n console.log("hodor!") \n};\nreturn $0;\n})();\n');
+        res.toString().should.equal('module.exports = (function() {var $0 = {};$0.Test = function(stack) { console.log("hodor!") };return $0;})();');
     });
 
     it('should emit operators', function() {
         var res = transpile.transpile(s.analyze(p.parse(l.lex('Test: +;'), 'hodor')));
-        res.should.equal('module.exports = (function() {\nvar $0 = {};\n$0.Test = function(stack) {\nstd.Add(stack);\n};\nreturn $0;\n})();\n');
+        res.toString().should.equal('module.exports = (function() {var $0 = {};$0.Test = function(stack) {std.Add(stack);};return $0;})();');
     });
 
     it('should emit built in functions', function() {
         var res = transpile.transpile(s.analyze(p.parse(l.lex('Test: dup;'), 'hodor')));
-        res.should.equal('var std = require("wort");\nmodule.exports = (function() {\nvar $0 = {};\n$0.Test = function(stack) {\nstd.Dup(stack);\n};\nreturn $0;\n})();\n');
+        res.toString().should.equal('var std = require("wort");module.exports = (function() {var $0 = {};$0.Test = function(stack) {std.Dup(stack);};return $0;})();');
     });
 
     it('should emit property accessors', function() {
         var res = transpile.transpile(s.analyze(p.parse(l.lex('Test: ->hodor;'), 'hodor')));
-        res.should.equal('module.exports = (function() {\nvar $0 = {};\n$0.Test = function(stack) {\nstd.Setvalobj(stack, "hodor");\n};\nreturn $0;\n})();\n');
+        res.toString().should.equal('module.exports = (function() {var $0 = {};$0.Test = function(stack) {std.Setvalobj(stack, "hodor");};return $0;})();');
     });
 
     it('should emit user defined words', function() {
         var res = transpile.transpile(s.analyze(p.parse(l.lex('-test: 2; test: -test;'), 'hodor')));
-        res.should.equal('module.exports = (function() {\nvar $0 = {};\nfunction _test(stack) {\nstack.push(2);\n}\n$0.test = function(stack) {\n_test(stack);\n};\nreturn $0;\n})();\n');
+        res.toString().should.equal('module.exports = (function() {var $0 = {};function _test(stack) {stack.push(2);}$0.test = function(stack) {_test(stack);};return $0;})();');
     });
 
     it('should emit quotations', function() {
         var res = transpile.transpile(s.analyze(p.parse(l.lex('Test: [2];'), 'hodor')));
-        res.should.equal('module.exports = (function() {\nvar $0 = {};\n$0.Test = function(stack) {\nstack.push([2]);\n};\nreturn $0;\n})();\n');
+        res.toString().should.equal('module.exports = (function() {var $0 = {};$0.Test = function(stack) {stack.push([2]);};return $0;})();');
 
         res = transpile.transpile(s.analyze(p.parse(l.lex('test: [dup];'), 'hodor')));
-        res.should.equal('var std = require("wort");\nmodule.exports = (function() {\nvar $0 = {};\n$0.test = function(stack) {\nstack.push([std.Dup]);\n};\nreturn $0;\n})();\n')
+        res.toString().should.equal('var std = require("wort");module.exports = (function() {var $0 = {};$0.test = function(stack) {stack.push([std.Dup]);};return $0;})();')
     });
 
     it('should emit stack shuffle operations', function() {
         var res = transpile.transpile(s.analyze(p.parse(l.lex('test: (ab-ba);'), 'hodor')));
-        res.should.equal('module.exports = (function() {\nvar $0 = {};\nfunction $shuffle0(stack) {\n' +
-            'var b = stack.pop();\nvar a = stack.pop();\nstack.push(b);\nstack.push(a);\n' +
-            '}\n$0.test = function(stack) {\n$shuffle0(stack);\n};\nreturn $0;\n})();\n');
+        res.toString().should.equal('module.exports = (function() {var $0 = {};function $shuffle0(stack) {' +
+            'var b = stack.pop();var a = stack.pop();stack.push(b);stack.push(a);' +
+            '}$0.test = function(stack) {$shuffle0(stack);};return $0;})();');
     });
 
     it('should call public main if present', function() {
         var res = transpile.transpile(s.analyze(p.parse(l.lex('main: 2;'), 'hodor')));
-        res.should.equal('var $1 = (function() {\nvar $0 = {};\n$0.main = function(stack) {\nstack.push(2);\n};\nreturn $0;\n})();\n$1.main([]);\n');
+        res.toString().should.equal('var $1 = (function() {var $0 = {};$0.main = function(stack) {stack.push(2);};return $0;})();$1.main([]);');
     });
 });
